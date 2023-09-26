457570d156ed9a92fd2877fbcdcf1b04445f07ad
MJUDDIN01 <mjuddin88@gmail.com>
2023-09-26 11:12:09 +0100

    Initial Commit

---
+++ PhoneMarket/PhoneMarketApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhoneMarketApp.swift
+//  PhoneMarket
+//
+//  Created by Jasim Uddin on 26/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct PhoneMarketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
