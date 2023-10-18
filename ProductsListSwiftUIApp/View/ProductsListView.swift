71bcee91ce04412a3406e497f40dd84785fe7b7f
MJUDDIN01 <mjuddin88@gmail.com>
2023-10-18 15:30:52 +0100

    Added the search logic

--- ProductsListSwiftUIApp/View/ProductsListView.swift
+++ ProductsListSwiftUIApp/View/ProductsListView.swift
@@ -9,7 +9,9 @@ import SwiftUI
 
 struct ProductsListView: View {
     @StateObject private var viewModel: ProductsListViewModel
-    
+    // Assuming Product is the correct product type
+    @State private var searchResult: [Products] = []
+    @State private var noMatchingSearchItemFound: Bool = false
     init() {
         let networkingManager = NetworkingManager.shared
         self._viewModel = StateObject(wrappedValue: ProductsListViewModel(productFetcher: networkingManager))
@@ -17,24 +19,42 @@ struct ProductsListView: View {
     
     var body: some View {
         ZStack {
-                List(viewModel.products) { product in
-                    NavigationLink {
-                        ProductsDetailView(product: product)
-                    } label: {
-                        ProductsRow(product: product)
+            List(searchResult.isEmpty ? viewModel.products : searchResult, id: \.id) { product in
+                NavigationLink(destination: ProductsDetailView(product: product)) {
+                    ProductsRow(product: product)
+                }
+            }
+            .searchable(text: $viewModel.searchText) {
+                ForEach(viewModel.products, id: \.id) { product in
+                    Text("You might want \(product.brand)")
+                        .searchCompletion(product.brand)
+                }
+            }
+            .onChange(of: viewModel.searchText) { searchText in
+                if searchText.isEmpty {
+                    // Clear the search results if the search text is empty/
+                    searchResult = []
+                    noMatchingSearchItemFound = false
+                } else {
+                    searchResult = viewModel.products.filter {
+                        $0.brand.localizedCaseInsensitiveContains(searchText)
                     }
+                    noMatchingSearchItemFound = searchResult.isEmpty
+
+                    // GET products from viewModel.products that appear in searchResult
+                    let productIdsInSearch = Set(searchResult.map { $0.id })
+                    viewModel.products = viewModel.products.filter { productIdsInSearch.contains($0.id) }
                 }
-                .navigationTitle("Latest Products")
-                .searchable(text: $viewModel.searchText)
-                .onAppear { viewModel.refreshProduct() }
-                .refreshable {
-                    viewModel.pullToRefresh()
-                }
-                if viewModel.isLoading { ProductsLoadingView() }
             }
-            .alert(item: $viewModel.alertData) { alertItem in
-                return Alert(title: Text(alertItem.title), message: Text(alertItem.message),
-                             dismissButton: .default(Text(alertItem.dismissButton)))
+            .onAppear { viewModel.refreshProduct() }
+            .refreshable {
+                viewModel.pullToRefresh()
             }
+            if viewModel.isLoading { ProductsLoadingView() }
+        }
+        .alert(item: $viewModel.alertData) { alertItem in
+            return Alert(title: Text(alertItem.title), message: Text(alertItem.message),
+                         dismissButton: .default(Text(alertItem.dismissButton)))
+        }
     }
 }
--- ProductsListSwiftUIApp_Tests/PhoneMarketListViewModel_Tests.swift
+++ ProductsListSwiftUIApp_Tests/PhoneMarketListViewModel_Tests.swift
@@ -12,9 +12,10 @@ class ProductsListViewModelTests: XCTestCase {
     
     func testIsLoadingInitiallyFalse() {
         // Given
+//        let isLoading = false
         let productFetcher = MockProductFetcher()
         // When
-        let viewModel = ProductsListViewModel(productFetcher: productFetcher)
+        let viewModel = ProductsListViewModel(productFetcher: productFetcher, isLoading: false)
         // Then
         XCTAssertFalse(viewModel.isLoading)
     }
@@ -23,7 +24,7 @@ class ProductsListViewModelTests: XCTestCase {
         // Given
         let productFetcher = MockProductFetcher()
         // When
-        let viewModel = ProductsListViewModel(productFetcher: productFetcher)
+        let viewModel = ProductsListViewModel(productFetcher: productFetcher, isLoading: true)
         // Simulate a refresh
         viewModel.refreshProduct()
         // Then
